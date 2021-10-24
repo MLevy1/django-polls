805f72d4dbe36934604dca89aa3a8649c4d435a9
MLevy1 <mslevy1@gmail.com>
2021-10-24 05:02:25 -0400

    add activity detail view

--- timedb/urls.py
+++ timedb/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:act_id>/', views.act_detail, name'deatil')
 ]
--- timedb/views.py
+++ timedb/views.py
@@ -6,3 +6,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>A New and Improved Time Database!</h1>")
+
+
+def act_detail(request, act_id):
+    return HttpResponse("You're looking at activity %s." %act_id)
