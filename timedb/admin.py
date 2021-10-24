476255e929a0f27835b48417e179321c64080688
MLevy1 <mslevy1@gmail.com>
2021-10-24 04:20:03 -0400

    update admin

--- timedb/admin.py
+++ timedb/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Activity, Event, SubProj
+
+admin.ste.register(Activty)
+admin.ste.register(Event)
+admin.ste.register(SubProj)
